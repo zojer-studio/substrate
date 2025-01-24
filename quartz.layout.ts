adb7ad2a3c30b7ee0c8bf834ea2b4a5a67f1be0e
Aaron Pham <contact@aarnphm.xyz>
2025-01-23 22:33:46 -0500

    feat(graph): enable radial mode
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,7 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({ globalGraph: { enableRadial: true } }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],

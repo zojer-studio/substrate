fd8d80f1ddca20e897b630fd125e1f1cc66e297f
Aaron Pham <contact@aarnphm.xyz>
2025-01-24 19:41:13 -0500

    chore: ignore enableRadial on main docs
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,7 +30,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph({ globalGraph: { enableRadial: true } }),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
